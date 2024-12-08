307e58dfa7ecb7fbe671ffc979b6ccd13fa2d2e4
Nico <nlovejoy@ondema.io>
2024-12-08 11:53:11 -0800

    sneak up on adding Routes, still working

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navigation from "../Navigation";
+import LandingPage from "../Landing";
+import SignUpPage from "../SignUp";
+import SignInPage from "../SignIn";
+import PasswordForgetPage from "../PasswordForget";
+import HomePage from "../Home";
+import AccountPage from "../Account";
+import AdminPage from "../Admin";
+
+import * as ROUTES from "../../constants/roles";
 
 const App = () => (
   <Router>
-    <Navigation />
+    <div>
+      <Navigation />
+    </div>
   </Router>
 );
 
