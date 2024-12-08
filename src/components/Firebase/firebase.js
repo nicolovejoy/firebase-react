72e8f0b26bd6a678fbe242ebcad0ae62ad4f2ca6
Nico <nlovejoy@ondema.io>
2024-12-08 11:26:22 -0800

    add firebaseConfig

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDKzpCdPtKDUSCANz64ywga57EiOylxI4Q",
+  authDomain: "my-first-firebase-projec-16f02.firebaseapp.com",
+  projectId: "my-first-firebase-projec-16f02",
+  storageBucket: "my-first-firebase-projec-16f02.firebasestorage.app",
+  messagingSenderId: "309499205901",
+  appId: "1:309499205901:web:19c5d27a483fc935167844",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
