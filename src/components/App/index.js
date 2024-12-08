de7abd2e3cc252f4b66f16e90a605d4ac4388bc4
Nico <nlovejoy@ondema.io>
2024-12-08 11:48:06 -0800

    nav links showing

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import Navigation from "../Navigation";
 
 const App = () => (
-  <div>
-    <h1>App Happy App Nappy Tap</h1>
-  </div>
+  <Router>
+    <Navigation />
+  </Router>
 );
 
 export default App;
