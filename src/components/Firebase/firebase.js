ff5f804b8be229c8cc0f2d8395de32fa5fb855ce
Nico <nlovejoy@ondema.io>
2024-12-08 12:39:37 -0800

    start setting up firebase context

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,17 +1,20 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDKzpCdPtKDUSCANz64ywga57EiOylxI4Q",
-  authDomain: "my-first-firebase-projec-16f02.firebaseapp.com",
-  projectId: "my-first-firebase-projec-16f02",
-  storageBucket: "my-first-firebase-projec-16f02.firebasestorage.app",
-  messagingSenderId: "309499205901",
-  appId: "1:309499205901:web:19c5d27a483fc935167844",
+import app from "firebase/app";
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+  }
+}
+
+export default Firebase;
+
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(config);
