ca3d676d84a64cdd09ae3ecc25cc850a6a565732
Nico <nlovejoy@ondema.io>
2024-12-10 16:23:35 -0800

    fixed firebase env variable issue

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,12 +1,17 @@
-import app from "firebase/app";
+// import app from "firebase/app";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"; // Or other Firebase services you need
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
+
+const app = initializeApp(config);
+// const auth = getAuth(app);
 
 class Firebase {
   constructor() {
