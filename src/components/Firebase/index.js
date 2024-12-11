4c180e9829b1a12597d34bb43b4990d3100810c4
Nico <nlovejoy@ondema.io>
2024-12-10 17:11:20 -0800

    Resolved merge conflicts

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,6 +1,41 @@
-import FirebaseContext from "./context";
-import Firebase from "./firebase";
+import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+import React from "react";
 
-export default Firebase;
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+};
 
-export { FirebaseContext };
+const FirebaseContext = React.createContext(null);
+
+// // initialize Firebase
+const app = initializeApp(firebaseConfig);
+// // Initialize Firebase Authentication and get a reference to the service
+// const auth = getAuth(app);
+// const doCreateUserWithEmailAndPassword = (email, password) =>
+//   auth.createUserWithEmailAndPassword(email, password);
+
+// const doSignInWithEmailAndPassword = (email, password) =>
+//   auth.signInWithEmailAndPassword(email, password);
+// const doSignOut = () => auth.signOut();
+// const doPasswordReset = (email) => auth.sendPasswordResetEmail(email);
+
+// const doPasswordUpdate = (password) =>
+//   auth.currentUser.updatePassword(password);
+
+const FirebaseProvider = ({ children }) => {
+  if (app && app.apps && !app.apps.length) {
+    app.initializeApp(firebaseConfig);
+  }
+  return (
+    <FirebaseContext.Provider value={app}>{children}</FirebaseContext.Provider>
+  );
+};
+
+export { FirebaseProvider };
--- src/index.js
+++ src/index.js
@@ -2,17 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
-import reportWebVitals from "./reportWebVitals";
-import Firebase, { FirebaseContext } from "./components/Firebase";
+import { FirebaseProvider } from "./components/Firebase";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <FirebaseProvider>
+      <App />
+    </FirebaseProvider>
+    ,
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
