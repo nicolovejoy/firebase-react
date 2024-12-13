d2d94759869374e9b02d865064c9bf281a656116
Nico <nlovejoy@ondema.io>
2024-12-13 07:46:39 -0800

    move firebaseConfig to constants/

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,23 +1,14 @@
 import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import React from "react";
-
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_ID,
-};
+import { firebaseConfig } from "../../constants/firebaseConfig";
 
 const FirebaseContext = React.createContext(null);
 
-// // initialize Firebase
+// initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 // // Initialize Firebase Authentication and get a reference to the service
-// const auth = getAuth(app);
 // const doCreateUserWithEmailAndPassword = (email, password) =>
 //   auth.createUserWithEmailAndPassword(email, password);
 
@@ -30,9 +21,6 @@ const app = initializeApp(firebaseConfig);
 //   auth.currentUser.updatePassword(password);
 
 const FirebaseProvider = ({ children }) => {
-  if (app && app.apps && !app.apps.length) {
-    app.initializeApp(firebaseConfig);
-  }
   return (
     <FirebaseContext.Provider value={app}>{children}</FirebaseContext.Provider>
   );
